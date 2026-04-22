959d322371193e2d290d5970721e261c1bdd369b
Jeroen van Bemmel <jvb127@gmail.com>
2026-04-22 12:31:20 -0500

    Add PSAG certify page and simplify plugin redirect

--- browser_plugin/background.js
+++ browser_plugin/background.js
@@ -1,5 +1,5 @@
 const MENU_ID = "philatelister-centering-analyze-image";
-const ANALYZE_BASE_URL = "http://localhost:8000/public/test/opencv_centering.html";
+const ANALYZE_BASE_URL = "https://exergy-connect.github.io/PhilateLister/test/opencv_centering.html";
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.removeAll(() => {
@@ -25,18 +25,7 @@ chrome.contextMenus.onClicked.addListener(async (info) => {
   if (info.menuItemId !== MENU_ID) return;
   if (!info.srcUrl) return;
 
-  try {
-    const url = new URL(ANALYZE_BASE_URL);
-    url.searchParams.set("image_url", info.srcUrl);
-    await chrome.tabs.create({ url: url.toString() });
-  } catch (err) {
-    const url = new URL(chrome.runtime.getURL("analyze.html"));
-    url.searchParams.set(
-      "error",
-      err instanceof Error
-        ? err.message
-        : "Could not open the analyzer page. Ensure your local server is running on localhost:8000."
-    );
-    await chrome.tabs.create({ url: url.toString() });
-  }
+  const url = new URL(ANALYZE_BASE_URL);
+  url.searchParams.set("image_url", info.srcUrl);
+  await chrome.tabs.create({ url: url.toString() });
 });
